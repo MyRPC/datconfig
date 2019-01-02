529e896c92084b3488f35af144b37cd451255d22
Kyle Smith <keenonsalish@icloud.com>
2019-01-02 15:45:11 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+ function readFile (filePath) {
+    var data = fs.readFileSync(filePath)
+}
+function parse function (text) {
+    // Example input:
+    // set x to y
+    // set foo to bar
+    
+}
