c6188e0963226193be483409acec3ea364c76aaa
Kyle Smith <keenonsalish@icloud.com>
2019-01-02 16:35:36 -0400

    Removed stupidity

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
- function readFile (filePath) {
-    var data = fs.readFileSync(filePath)
+const fs = require('fs');
+const dotProp = require('dot-prop');
+function parseFile (filePath) {
+    var data = fs.readFileSync(filePath, {encoding: "utf8"});
+    var lines = data.replace("\r", "").split("\n");
+    var final = {}
+    lines.forEach((e) => {
+        var parsed = parse(e);
+        var path = parsed[0].replace(":", ".")
+        dotProp.set(final, path, parsed[1].replace("[", "").replace("]", ""));
+    })
+    return final
+
+
 }
-function parse function (text) {
-    // Example input:
-    // set x to y
-    // set foo to bar
+function parse (text) {
+    var valueMatch = /\[\w+\]/g
+    var keyMatch = /\w+:\w+/g
+    var value = text.match(valueMatch)[0]
+    var key = text.match(keyMatch)[0]
+    console.log(key, value)
+    return [key, value]
     
 }
+module.parse = parseFile
+
